004296b3bed05b0fad497edacddeb83c7b17c2ef
Junting <342766475@qq.com>
2019-04-30 14:51:11 +0800

    :sparkles: Chapter1 - 01. 基础类型

--- ch1/01.type.ts
+++ ch1/01.type.ts
@@ -1,3 +1,11 @@
+/**
+ * 基本类型
+ * @Author: Junting.liu
+ * @Date: 2019-04-30 14:42:23
+ * @Last Modified by:   Junting.liu
+ * @Last Modified time: 2019-04-30 14:42:23
+ */
+
 // 布尔值
 let isDone: boolean = false;
 
@@ -69,3 +77,45 @@ u2 = undefined;
 // 默认情况下 null 和 undefined 是所有类型的子类型。 就是说你可以把 null 和 undefined 赋值给 number 类型的变量。
 // * tsc type.ts --strictNullChecks 使用严格模式，null 和 undefined 只能赋值给 void 和它们各自
 // 如果想给一个变量支持多种类型可以采用：联合模式 （string | null | undefined）
+
+// never
+// 表示永远不存在的值的类型
+// 类型是任何类型的子类型，也可以赋值给任何类型；
+// 但没有任何类型是 never 的子类型； 即使 any 也不行
+
+// 使用场景1: 函数不能有返回值，使用抛出异常来处理
+function error(message: string):never {
+  throw new Error(message);
+}
+
+function fail() { // 推断的返回值类型为never
+  return error("Something failed");
+}
+
+// 使用场景2: 返回 never 的函数必须存在无法到达的终点
+function infiniteLoop(): never {
+  while(true) {}
+}
+
+// object
+// object表示非原始类型，也就是除number，string，boolean，symbol，null 或 undefined 之外的类型。
+
+// 声明
+declare function create(o: object | null): void;
+
+
+create({ prop: 0 }); // OK
+create(null); // OK
+
+create(42); // Error
+create("string"); // Error
+create(false); // Error
+create(undefined); // Error
+
+// 类型断言
+// 你很清楚的知道该值是什么类型，断言该值就是这个类型
+// 类型断言好比其它语言里的类型转换，但是不进行特殊的数据检查和解构。
+
+let someValue:any = 'this is a string';
+// let strLength: number = (<string>someValue).length; // 强制转化类型, 断言 someValue 是一个 string 类型
+let strLength: number = (someValue as string).length; // JSX 语法里只支持 as
---
+++ ch1/02.variable.ts
@@ -0,0 +1,15 @@
+/**
+ * 变量声明
+ * @Author: Junting.liu
+ * @Date: 2019-04-30 14:43:28
+ * @Last Modified by: Junting.liu
+ * @Last Modified time: 2019-04-30 14:44:49
+ */
+
+// var
+
+// let
+
+// const
+
+// var vs let
